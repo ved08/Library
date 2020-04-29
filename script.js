e1bd937e2d06a6f87ca68afe1361d5fa9c1d2e99
Vedvardhan <vedvardhan.g@gmail.com>
2020-04-29 11:30:01 +0530

    Major updates left..Beta ready

--- script.js
+++ script.js
@@ -1,29 +1,56 @@
-let myLibrary = new Array;
-function Book(title, author, pages, status){
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.status = status;
-  }
+let bookData = [];
+let counter = 0
+let title = document.getElementById('title');
+let author = document.getElementById('author');
+let pages = document.getElementById('pages');
 
-function addBookToLibrary(){
-  let bookTitle = document.getElementById('title').value;
-  let bookAuthor = document.getElementById('author').value;
-  let bookPages = document.getElementById('pages').value;
-  let bookStatus;
-  document.getElementById('status').checked == true ? bookStatus = true : bookStatus = false;
-  const userData = [bookTitle, bookAuthor, bookPages, bookStatus];
-  myLibrary.push(userData)
-  console.log(myLibrary)
-  
-}
-function render(){
-  let table = document.getElementById('table');
-  for(let book in myLibrary){
-   let row = table.insertRow();
-    for(let bookInfo in book){
-      let cell = row.insertCell()
-      cell.innerHTML = bookInfo;
-    }
+const root = document.getElementById('book-info');
+let row;
+let printTitle;
+let printAuthor;
+let printPages;
+let del;
+let read;
+let sno;
+
+function addNewBook(){
+  counter += 1;
+  let select = document.getElementById('select');
+  let option = select.options[select.selectedIndex].value
+  console.log(option)
+ 
+  let book = new Book(title.value, author.value, pages.value);
+  function Book(Btitle,Bauthor,Bpages){
+    this.title = Btitle,
+    this.author = Bauthor,
+    this.pages = Bpages
   }
+  bookData.push(book);
+  console.log(bookData)
+ 
+  row = document.createElement('tr');
+  row.setAttribute('data-index', counter)
+ 
+  printTitle = document.createElement('td');
+  printAuthor = document.createElement('td');
+  printPages = document.createElement('td');
+  
+  del = document.createElement('button')
+  del.setAttribute('onclick', 'deleteElem()');
+  del.setAttribute('data-index', counter)
+  del.textContent = 'Delete'
+ 
+  read = document.createElement('button');
+  read.setAttribute('onclick', 'read()');
+  read.textContent = 'Read the book'
+
+  printTitle.textContent = book.title;
+  printAuthor.textContent = book.author;
+  printPages.textContent = book.pages;
+  row.append(printTitle, printAuthor, printPages, option, del, read);
+  root.append(row)
+}
+function deleteElem() {
+let parentTag = event.target.parentElement;
+parentTag.remove();
 }
